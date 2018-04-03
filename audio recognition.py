ea942317194851a4878584bf5a4082a27cdcf9b1
jones640 <584083@bah.com>
2018-04-03 16:28:27 -0500

    Started into using Google Speech Recognition for the clip but was unable to use, most likely due to needing an API key from Google.  Additionally started into the conversion of another file type to .wav for use with other file types to include .mp4 video files being converted to only Audio.

--- audio recognition.py
+++ audio recognition.py
@@ -3,6 +3,7 @@
 from pyaudio import *
 from pocketsphinx import *
 import speech_recognition as sr
+#from ffmpy import *
 #from SpeechRecognition import *
 import Tkinter # Python GUI package
 import tkFileDialog # for Dialog Box
@@ -25,6 +26,23 @@ ftypes = [
     ('All files', '*'), 
 ]
 
+"""source_file = tkFileDialog.askopenfilename(parent=gui, initialdir=initialdir, title= 'Select a video file to be analyzed', filetypes=ftypes)
+
+init_filename_list = source_file.split('/')
+init_filename = init_filename_list[-1]
+filename_base_list = init_filename.split('.')
+filename_base = filename_base_list[0]
+
+print(str(filename_base) + ".wav")
+
+ff = ffmpy.FFmpeg(
+    inputs={source_file: None}, 
+    outputs={str(filename_base) + ".wav": None}
+)
+ff.run()
+"""
+# for just Audio file in .wav form
+
 source_audio_file = tkFileDialog.askopenfilename(parent=gui, initialdir=initialdir, title= 'Select an audio file to be analyzed', filetypes=ftypes)
 
 # use the source_audio_file as the audio source
@@ -34,9 +52,22 @@ with sr.AudioFile(source_audio_file) as source:
 
 print(str(source_audio_file))
 
+"""try:
+    print("Google thinks you said:  '" + r. recognize_google(audio) + "'")
+except sr.UnknownValueError:
+    print("Google could not understand audio")
+except sr.RequestError as e:
+    print("Could not complete request for Google Speech Recognition service; {0}".format(e))
+
+ 
+print("Starting Sphinx")
+"""
+
 try:
     print("Sphinx thinks you said:  '" + r.recognize_sphinx(audio) + "'")
 except sr.UnknownValueError:
     print("Sphinx could not understand audio")
 except sr.RequestError as e:
     print("Sphinx error; {0}".format(e))
+    
+
